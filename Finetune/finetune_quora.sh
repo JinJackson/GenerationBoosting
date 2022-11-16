742b6a74921e4f1eb3bd9088a0c6792c2ad6840d
Jackson <635725626@qq.com>
2022-11-16 15:38:02 +0800

    quora

--- Finetune/finetune_quora.sh
+++ Finetune/finetune_quora.sh
@@ -8,8 +8,8 @@ epochs=3
 seed=4096
 learning_rate='2.5e-5'
 
-boosting_ratio=0.5
-saving_steps=1000
+boosting_ratio=0.1
+saving_steps=500
 
 exp_type=boosting_notshuffshell_col1_col2_ratio_afterwarmup_reproduct_test_cu13
 
@@ -21,7 +21,7 @@ dev_file="../Data/$dataset/clean/dev_clean.txt"
 test_file="../Data/$dataset/clean/test_clean.txt"
 
 
-output_dir="/data1/zljin/experiments/Paraphrase/Finetune/result_test/$dataset/$exp_type/$model_name/""bs"$batch_size"_epoch"$epochs"_lr"$learning_rate"_savingsteps"$saving_steps"_seed"$seed"_ratio"$boosting_ratio"2"/
+output_dir="/data1/zljin/experiments/Paraphrase/Finetune/result/$dataset/$exp_type/$model_name/""bs"$batch_size"_epoch"$epochs"_lr"$learning_rate"_savingsteps"$saving_steps"_seed"$seed"_ratio"$boosting_ratio/
 
 
 echo $train_file
@@ -44,6 +44,7 @@ CUDA_VISIBLE_DEVICES=$1 python run_finetune.py \
 --boosting_train \
 --boosting_col1 \
 --boosting_col2 \
+--boosting_origin \
 --boosting_ratio $boosting_ratio \
 --warmup_steps 0.1
-# --boosting_origin \
+
